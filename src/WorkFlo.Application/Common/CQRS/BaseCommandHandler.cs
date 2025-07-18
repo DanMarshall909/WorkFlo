d7238ced641371e17f993a6fca721ac30022bd44
Dan <danmarshall909@gmail.com>
2025-07-19 00:17:12 +1000

    fix: add IError constraint to base handler methods

--- src/WorkFlo.Application/Common/CQRS/BaseCommandHandler.cs
+++ src/WorkFlo.Application/Common/CQRS/BaseCommandHandler.cs
@@ -1,5 +1,6 @@
 using MediatR;
 using WorkFlo.Domain.Common;
+using WorkFlo.Domain.Common.Errors;
 
 namespace WorkFlo.Application.Common.CQRS;
 
@@ -8,10 +9,12 @@ public abstract class BaseCommandHandler<TCommand, TResponse> : IRequestHandler<
 {
     public abstract Task<TResponse> Handle(TCommand request, CancellationToken cancellationToken);
     
-    protected TypeSafeResult<T, TError> Success<T, TError>(T value) =>
+    protected TypeSafeResult<T, TError> Success<T, TError>(T value) 
+        where TError : IError =>
         TypeSafeResult<T, TError>.Success(value);
         
-    protected TypeSafeResult<T, TError> Failure<T, TError>(TError error) =>
+    protected TypeSafeResult<T, TError> Failure<T, TError>(TError error) 
+        where TError : IError =>
         TypeSafeResult<T, TError>.Failure(error);
 }
 
--- src/WorkFlo.Application/Common/CQRS/BaseQueryHandler.cs
+++ src/WorkFlo.Application/Common/CQRS/BaseQueryHandler.cs
@@ -1,5 +1,6 @@
 using MediatR;
 using WorkFlo.Domain.Common;
+using WorkFlo.Domain.Common.Errors;
 
 namespace WorkFlo.Application.Common.CQRS;
 
@@ -8,9 +9,11 @@ public abstract class BaseQueryHandler<TQuery, TResponse> : IRequestHandler<TQue
 {
     public abstract Task<TResponse> Handle(TQuery request, CancellationToken cancellationToken);
     
-    protected TypeSafeResult<T, TError> Success<T, TError>(T value) =>
+    protected TypeSafeResult<T, TError> Success<T, TError>(T value) 
+        where TError : IError =>
         TypeSafeResult<T, TError>.Success(value);
         
-    protected TypeSafeResult<T, TError> Failure<T, TError>(TError error) =>
+    protected TypeSafeResult<T, TError> Failure<T, TError>(TError error) 
+        where TError : IError =>
         TypeSafeResult<T, TError>.Failure(error);
 }
